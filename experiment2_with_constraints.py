0c03f9072a3dac6c6fe7ae5f90ab57b8e44588ba
joannagrzyb <joanna.grzyb@pwr.edu.pl>
2022-11-20 10:39:42 +0100

    ex2

--- experiment2_with_constraints.py
+++ experiment2_with_constraints.py
@@ -39,16 +39,16 @@ n_proccess = 16
 methods = {
     "DE_Forest_constr":
         DifferentialEvolutionForest(base_classifier=base_estimator, n_classifiers=15, metric_name="BAC", bootstrap=True, random_state_cv=222, p_size=107, constraints=True),
-    # "RandomFS":
-    #     RandomFS(base_classifier=base_estimator, n_classifiers=15, bootstrap=False, max_features_selected=True),
-    # "RandomFS_b":
-    #     RandomFS(base_classifier=base_estimator, n_classifiers=15, bootstrap=True, max_features_selected=True),
-    # "DT":
-    #     DecisionTreeClassifier(random_state=1234),
-    # "RF":
-    #     RandomForestClassifier(random_state=1234, n_estimators=15, bootstrap=False, max_depth=2, max_leaf_nodes=4),
-    # "RF_b":
-        # RandomForestClassifier(random_state=0, n_estimators=15, bootstrap=True),
+    "RandomFS":
+        RandomFS(base_classifier=base_estimator, n_classifiers=15, bootstrap=False, max_features_selected=True),
+    "RandomFS_b":
+        RandomFS(base_classifier=base_estimator, n_classifiers=15, bootstrap=True, max_features_selected=True),
+    "DT":
+        DecisionTreeClassifier(random_state=1234),
+    "RF":
+        RandomForestClassifier(random_state=1234, n_estimators=15, bootstrap=False, max_depth=2, max_leaf_nodes=4),
+    "RF_b":
+        RandomForestClassifier(random_state=0, n_estimators=15, bootstrap=True),
 
     # "DE_Forest_AUC":
     #     DifferentialEvolutionForest(base_classifier=base_estimator, n_classifiers=10, metric_name="AUC", alpha=1, bootstrap=False, n_proccess=n_proccess, random_state_cv=222),
@@ -76,9 +76,9 @@ n_repeats = 5
 rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=111)
 n_folds = n_splits * n_repeats
 
-DATASETS_DIR = "dtest/"
+# DATASETS_DIR = "dtest/"
 # DATASETS_DIR = "ds10/"
-# DATASETS_DIR = "datasets/"
+DATASETS_DIR = "datasets/"
 dataset_paths = []
 for root, _, files in os.walk(DATASETS_DIR):
     for filename in filter(lambda _: _.endswith('.dat'), files):
@@ -190,7 +190,7 @@ def compute(dataset_id, dataset_path):
 
 
 # Multithread; n_jobs - number of threads, where -1 all threads, safe for my computer 2
-Parallel(n_jobs=1)(
+Parallel(n_jobs=-1)(
                 delayed(compute)
                 (dataset_id, dataset_path)
                 for dataset_id, dataset_path in enumerate(dataset_paths)
